8a8305fac42c10fb0796c574bb9886b534719eff
hualigushi <hualigushi@163.com>
2023-10-03 19:32:10 +0800

    feat: 静态代码分析

---
+++ my-code-analysis-ts/demo.js
@@ -0,0 +1,42 @@
+const tsCompiler = require('typescript');              // TS编译器
+
+// 待分析代码片段字符串
+const tsCode = `import { app } from 'framework';                    
+
+const dataLen = 3;
+let name = 'iceman';
+
+if(app){
+    console.log(name);
+}
+
+function getInfos (info: string) {
+    const result = app.get(info);
+    return result;
+}`;
+
+// 获取AST
+const ast = tsCompiler.createSourceFile('xxx', tsCode, tsCompiler.ScriptTarget.Latest, true);
+
+const apiMap={}
+
+function walk(node){
+    tsCompiler.forEachChild(node, walk)
+    console.log("walk ~ node:", node)
+    
+    const line = ast.getLineAndCharacterOfPosition(node.getStart()).line+1
+    if(tsCompiler.isIdentifier(node) && node.escapedText === 'app'){
+        if(Object.keys(apiMap).includes(node.escapedText)){
+            apiMap[node.escapedText].callNum++
+            apiMap[node.escapedText].callLines.push(line)
+        }else{
+            apiMap[node.escapedText] = {}
+            apiMap[node.escapedText].callNum=1
+            apiMap[node.escapedText].callLines=[]
+            apiMap[node.escapedText].callLines.push(line)
+        }
+    }
+}
+
+walk(ast)
+console.log(apiMap)
